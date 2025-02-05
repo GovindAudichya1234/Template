35fca69c95cdb322d3b820b6e1bf59819fdb4e15
GovindAudichya1234 <govind.audichya@hubblehox.com>
2025-02-05 17:18:36 +0530

    Update TemplateShifter.py

--- TemplateShifter.py
+++ TemplateShifter.py
@@ -53,7 +53,7 @@ def apply_formulas_to_range(file_path, col_range, row_range, review_col, Rev):
         "Question Accuracy", "Question Distribution", "Answer Accuracy", "Answer Explanation",
         "Tagging bloom level", "Tagging complexity level", "Distractors", "Learning Outcome",
         "No Repetition of PR Questions", "Topic Tagging", "Language and Grammar",
-        "No Plagiarism", "Copy Editing"
+        "Copy Editing"
     ]
     
     # Write headers in row 2
@@ -110,7 +110,7 @@ def apply_formulas_to_range(file_path, col_range, row_range, review_col, Rev):
     f'=IF(SUM(ISNUMBER(SEARCH("Repq:", {review_col}ROW)) + ISNUMBER(SEARCH("QR:", {review_col}ROW)) + ISNUMBER(SEARCH("Reject:", {review_col}ROW)))>0, "No", "Yes")',
     f'=IF(SUM(ISNUMBER(SEARCH("TopicT:", {review_col}ROW)) + ISNUMBER(SEARCH("TT:", {review_col}ROW)) + ISNUMBER(SEARCH("Reject:", {review_col}ROW)))>0, "No", "Yes")',
     f'=IF(SUM(ISNUMBER(SEARCH("Lang:", {review_col}ROW)) + ISNUMBER(SEARCH("LG:", {review_col}ROW)) + ISNUMBER(SEARCH("Reject:", {review_col}ROW)))>0, "No", "Yes")',
-    f'=IF(SUM(ISNUMBER(SEARCH("Plag:", {review_col}ROW)) + ISNUMBER(SEARCH("LG:", {review_col}ROW)) + ISNUMBER(SEARCH("Reject:", {review_col}ROW)))>0, "No", "Yes")',
+    
     f'=IF(SUM(ISNUMBER(SEARCH("Ced:", {review_col}ROW)) + ISNUMBER(SEARCH("CE:", {review_col}ROW)) + ISNUMBER(SEARCH("Reject:", {review_col}ROW)))>0, "No", "Yes")'
 ]
 
